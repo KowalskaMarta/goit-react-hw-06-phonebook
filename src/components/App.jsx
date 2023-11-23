da197500d94e54425f3c5156da9d039476a8ba2e
Kowalska Marta <kamarta0991@gmail.com>
2023-11-23 22:42:01 +0100

    add Phonebook06 and readme.md

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+
+const App = () => {
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ContactForm />
+      <Filter />
+      <ContactList />
+    </>
   );
 };
+
+export default App;
